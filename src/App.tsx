cada8b358455623675075d168e4a79e8d8b957a1
Nabeshima <a_nabeshima@currentia.co.jp>
2021-02-18 15:57:44 +0900

    Hello World で動くところまで出来たのでコミット

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import './App.css';
+
+const App: FC = () => (
+  <div className="container">
+    <header>
+      <h1>Hello World!</h1>
+    </header>
+  </div>
+);
+
+export default App;
